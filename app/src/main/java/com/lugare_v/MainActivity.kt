822162bca63a942743ade095737b4ae4849b0ce6
JohanV <johanv20@hotmail.com>
2022-09-20 18:09:55 -0600

    Version Semana 1

---
+++ app/src/main/java/com/lugare_v/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.lugare_v
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lugare_v.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        ////inicializar la autenticacion
+         FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+
+        ///difinir el boton
+
+        binding.btAdd.setOnClickListener{hacerRegistro()}
+
+
+        binding.btLogin.setOnClickListener{hacerLogin()}
+    }
+
+    private fun hacerRegistro() {
+        // recupero la info de los campos
+        val email = binding.etCorreo.text.toString()
+        val contra = binding.etContraseA.text.toString()
+
+        //utlizo el auth para el registro
+
+        auth.createUserWithEmailAndPassword(email,contra)
+            .addOnCompleteListener(this) { task->
+                if (task.isSuccessful){
+                    val user = auth.currentUser
+                    refresca(user)
+                }else {
+                    Toast.makeText(baseContext,"ERROR",Toast.LENGTH_LONG).show()
+                    refresca(null)
+                }
+            }
+    }
+
+    private fun refresca(user: FirebaseUser?) {
+    if(user != null) {
+        val intent = Intent(this,Principal::class.java)
+        startActivity(intent)  }
+    }
+
+    private fun hacerLogin() {
+        TODO("Not yet implemented")
+    }
+}
+
